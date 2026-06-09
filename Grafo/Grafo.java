5991c4a969bff9aa122cc024948804f033f650c7
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-06-09 20:28:10 -0300

    chore: retirando pseudocódigos

--- Grafo/Grafo.java
+++ Grafo/Grafo.java
@@ -16,15 +16,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public List<Vertice<TV, TA>> finalVertices(Aresta<TV, TA> e) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * criar vetor de tamanho 2
-         * vetor[0] = origem
-         * vetor[1] = destino
-         * retornar vetor
-         */
         List<Vertice<TV, TA>> verticesAresta = new ArrayList<>();
         verticesAresta.add(e.getOrigem());
         verticesAresta.add(e.getDestino());
@@ -34,19 +25,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public Vertice<TV, TA> oposto(Vertice<TV, TA> v, Aresta<TV, TA> e) throws RuntimeException{
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * se v == origem
-         *      retornar destino
-         *
-         * senão se v == destino
-         *      retornar origem
-         *
-         * senão
-         *      lançar exceção
-         */
         if (v == e.getOrigem()){
            return e.getDestino();
         }
@@ -60,18 +38,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public boolean ehAdjacente(Vertice<TV, TA> v, Vertice<TV, TA> w) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * percorrer arestas incidentes de v
-         *
-         * para cada aresta
-         *      se conecta v e w
-         *          retornar true
-         *
-         * retornar false
-         */
         for (Aresta<TV, TA> aresta : v.getArestasIncidentes()) {
             if ((aresta.getOrigem() == v && aresta.getDestino() == w) ||
                 (aresta.getOrigem() == w && aresta.getDestino() == v)){
@@ -83,36 +49,16 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public void substituir(Vertice<TV, TA> v, TV x) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * v.setElemento(x)
-         */
         v.setElemento(x);
     }
 
     @Override
     public void substituir(Aresta<TV, TA> e, TA x) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * e.setElemento(x)
-         */
         e.setElemento(x);
     }
 
     @Override
     public Vertice<TV, TA> inserirVertice(TV o) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * criar novo vértice
-         * adicionar na lista de vértices
-         * retornar vértice criado
-         */
         Vertice<TV, TA> novoVertice = new Vertice<>(o);
         this.vertices.add(novoVertice);
         return novoVertice;
@@ -120,19 +66,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public Aresta<TV, TA> inserirAresta(Vertice<TV, TA> v, Vertice<TV, TA> w, TA o) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * criar nova aresta não direcionada
-         *
-         * adicionar na lista global de arestas
-         *
-         * adicionar nas listas de incidência
-         * de v e de w
-         *
-         * retornar aresta criada
-         */
         Aresta<TV, TA> novaAresta = new Aresta<>(v, w, o, false);
         this.arestas.add(novaAresta);
         v.getArestasIncidentes().add(novaAresta);
@@ -142,19 +75,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public TV removerVertice(Vertice<TV, TA> v) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * obter todas as arestas incidentes
-         *
-         * para cada aresta
-         *      removerAresta(aresta)
-         *
-         * remover vértice da coleção
-         *
-         * retornar elemento armazenado
-         */
         TV verticeRemovido = v.getElemento();
         List<Aresta<TV, TA>> arestasIncidentes = new ArrayList<>(v.getArestasIncidentes());
         for (Aresta<TV, TA> aresta : arestasIncidentes){
@@ -166,20 +86,6 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public TA removerAresta(Aresta<TV, TA> e) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * remover da lista global
-         *
-         * remover da lista de incidência
-         * do vértice origem
-         *
-         * remover da lista de incidência
-         * do vértice destino
-         *
-         * retornar elemento armazenado
-         */
         TA arestaRemovida = e.getElemento();
         this.arestas.remove(e);
         e.getOrigem().getArestasIncidentes().remove(e);
@@ -189,65 +95,26 @@ public class Grafo<TV, TA> implements IGrafo<TV, TA> {
 
     @Override
     public List<Aresta<TV, TA>> arestasIncidentes(Vertice<TV, TA> v) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * retornar coleção de arestas
-         * associadas ao vértice
-         */
-
         return v.getArestasIncidentes();
     }
 
     @Override
     public Collection<Vertice<TV, TA>> vertices() {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * retornar lista de vértices
-         */
         return this.vertices;
     }
 
     @Override
     public Collection<Aresta<TV, TA>> arestas() {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * retornar lista de arestas
-         */
         return this.arestas;
     }
 
     @Override
     public boolean ehDirecionada(Aresta<TV, TA> e) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * retornar atributo direcionada
-         */
         return e.ehDirecionada();
     }
 
     @Override
     public Aresta<TV, TA> inserirArestaDirecionada(Vertice<TV, TA> origem, Vertice<TV, TA> destino, TA elemento) {
-
-        /*
-         * PSEUDOCÓDIGO
-         *
-         * criar aresta direcionada
-         *
-         * adicionar à lista global
-         *
-         * adicionar à lista de incidência
-         * da origem e destino
-         *
-         * retornar aresta criada
-         */
         Aresta<TV, TA> novaAresta = new Aresta<>(origem, destino, elemento, true);
         this.arestas.add(novaAresta);
         novaAresta.getOrigem().getArestasIncidentes().add(novaAresta);
