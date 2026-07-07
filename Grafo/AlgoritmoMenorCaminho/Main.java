fe4e7f2f6ed4eb91aeba59a8f4de0695f63a1f3f
Eduardo Medeiros <eduardo.aguiar@escolar.ifrn.edu.br>
2026-07-06 23:57:30 -0300

    refactor: alterando posição do arquivo dat

--- Grafo/AlgoritmoMenorCaminho/Main.java
+++ Grafo/AlgoritmoMenorCaminho/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        String arquivo = args.length > 0 ? args[0] : "labirinto.dat";
+        String arquivo = args.length > 0 ? args[0] : "../labirinto.dat";
 
         Labirinto labirinto;
         try {
